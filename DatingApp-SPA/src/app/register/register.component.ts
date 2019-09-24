d6be4c901bc9443368aa591e0a6da1296e8d6c22
Laura Rocha <laura_rocha_mx@hotmail.com>
2019-09-23 19:33:41 -0500

    Fin de la seccion 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,16 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authServicce: AuthService) { }
+  constructor(private authServicce: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authServicce.register(this.model).subscribe(() => {
-      console.log('Registro Exitoso');
+      this.alertify.success('Registro Exitoso');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
